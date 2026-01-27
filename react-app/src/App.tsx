94ae6beeacb7d0d0975528a056bd5e3cd2615b99
Daniel Cruz <danielfernandocruzpetoccini10@gmail.com>
2026-01-26 21:40:10 -0400

    pruebas

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,26 +1,28 @@
 import "./App.css";
-import IntroductionSection from "./components/IntroductionSection";
+/*import IntroductionSection from "./components/IntroductionSection";
 import Navbar2 from "./components/layout/Navbar2";
 import Shop from "./components/Shop";
 import Brand from "./components/Brand";
 import Instruction from "./components/Instruction";
-import Reviews from "./components/Reviews";
 import Footer from "./components/layout/Footer";
-import { useState } from "react";
+import { useState } from "react";*/
+import Reviews from "./components/Reviews";
+import "iframe-resizer/js/iframeResizer.contentWindow";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  //const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => setIsOpen(!isOpen);
+  //const handleClick = () => setIsOpen(!isOpen);
   return (
     <div className="App">
-      <Navbar2 state={isOpen} onClick={handleClick} />
-      <IntroductionSection />
+      {/*<Navbar2 state={isOpen} onClick={handleClick} />*/}
+      {/*<IntroductionSection />
       <Shop />
       <Brand />
       <Instruction />
+      
+      <Footer />*/}
       <Reviews />
-      <Footer />
     </div>
   );
 }
